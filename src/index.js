cd1c3cd774fba66aa46e39d1bdbfe1433d5bcecb
Yesenia <a.yeseniamartinez@gmail.com>
2022-04-12 12:32:54 +0200

    prettier config

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import { Auth0Provider } from "@auth0/auth0-react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
-import "./assets/styles/main.scss";
-import reportWebVitals from "./reportWebVitals";
-
+import { Auth0Provider } from '@auth0/auth0-react'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import './assets/styles/main.scss'
+import reportWebVitals from './reportWebVitals'
 
 const theme = createTheme({
     palette: {
-        type: "light",
+        type: 'light',
         primary: {
-            main: "#005c64",
+            main: '#005c64',
         },
         secondary: {
-            main: "#efd309",
+            main: '#efd309',
         },
     },
-});
+})
 ReactDOM.render(
     <React.StrictMode>
         <Auth0Provider
@@ -31,14 +30,14 @@ ReactDOM.render(
             <Router>
                 <ThemeProvider theme={theme}>
                     <App />
-                </ThemeProvider> 
+                </ThemeProvider>
             </Router>
         </Auth0Provider>
     </React.StrictMode>,
-    document.getElementById("root")
-);
+    document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
